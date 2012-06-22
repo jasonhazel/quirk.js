e77a3311b40e8391d5c745c214ffffd3b51f3bd5
Jason Hazel <me@jasonhazel.com>
2012-06-22 15:24:00 -0400

    initial commit

---
+++ framework.js
@@ -0,0 +1,87 @@
+(function(){  
+	var Quirk = function(selector)
+	{
+		//return document.getElementById(selector);
+		
+		var type = selector.charAt(0);
+		var element = selector.substring(1,selector.length);
+
+		switch(type)
+		{
+			case '#':
+				return Quirk.select.id(element);
+			break;
+			case '.':
+				return Quirk.select.class(element);
+			break;
+			case '<':
+				if(selector.charAt(selector.length - 1) == '>')
+				{
+					element = selector.substring(1,selector.length - 1);
+					alert(element);
+					return document.createElement(element);
+				}
+			break;
+			default:
+				return document.getElementsByTagName(selector)[0];
+
+			break;
+		}
+
+
+
+	}
+
+	// add a function to the wait queue.
+	Quirk.ready = function(func)
+	{
+		Quirk.ready.wait.push(func);
+	}
+
+	Quirk.ready.wait = []
+
+	Quirk.select = {
+		cache : {}, 
+		id : function(selector)
+		{
+			if(!Quirk.select.cache[selector])
+				Quirk.select.cache[selector] = document.getElementById(selector);
+			
+			return Quirk.select.cache[selector];
+		},
+		class : function(selector){
+
+
+		},
+		new : function(selector){
+
+
+		}
+
+	}
+
+	//once document is loaded, loop through the wait list and execute and functions.
+	document.onreadystatechange = function()
+	{
+		if(document.readyState == 'complete')
+		{
+			for(var i=0; i < Quirk.ready.wait.length; i++)
+				Quirk.ready.wait[i]();
+		}
+	}
+
+   	if(!window.$q){window.$q = window.Quirk = Quirk;}
+})();
+
+//usage tests
+$q.ready(function(){
+	console.debug($q('#header').innerHTML);
+	var body = $q('body');
+
+	var X = $q('<a>');
+	X.href = 'test';
+	X.innerHTML = 'Testing';
+	body.appendChild(X);
+
+});
+
