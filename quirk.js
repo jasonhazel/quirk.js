3768f11586b0010152b269457bc28ee07ba1fdd9
Jason Hazel <me@jasonhazel.com>
2012-07-12 15:45:48 -0400

    reworking.

--- quirk.js
+++ quirk.js
@@ -1,58 +1,27 @@
-(function(){  
+(function(){
 	var Quirk = function(selector)
-	{		
+	{
 		var type = selector.charAt(0);
 		var element = selector.substring(1,selector.length);
+
 
 		switch(type)
 		{
 			case '#':
 				e = Quirk.select.id(element);
-
-				// Quirk.select.id(element);
-				return ret;
 			break;
 			case '.':
-				return Quirk.select.class(element);
+				e = Quirk.select.class(element);
 			break;
 			case '<':
-				return Quirk.select.new(selector);
+				e = Quirk.select.new(selector);
 			break;
 			default:
 				return document.getElementsByTagName(selector)[0];
 			break;
 		}
+		return new qObj(e);
 	}
-
-	var qObj = function(element)
-	{
-		this.element = element;
-		this.text = function(str)
-		{
-			this.element.innerHTML = str;
-		}
-		this.attr = function()
-		{
-			switch(attributes.lengthd)
-			{
-				case 1:
-
-				break;
-
-
-			}
-		}
-	}
-
-
-	// add a function to the wait queue.
-	Quirk.ready = function(func)
-	{
-		Quirk.ready.wait.push(func);
-	}
-
-
-	Quirk.ready.wait = []
 
 	Quirk.select = {
 		cache : {}, 
@@ -77,16 +46,43 @@
 
 	}
 
+	// this is where most of the magic happens.
+	var qObj = function(element)
+	{
+		this.element = element;
+		this.text = function()
+		{
+			switch(arguments.length)
+			{
+				case 0:
+					return this.element.innerHTML;
+				break;
+				case 1:
+					this.element.innerHTML = arguments[0];
+					return this;
+				break;
+			}
+		}
+	}
+
+	// add a function to the wait queue.
+	Quirk.ready = function(func)
+	{
+		Quirk.ready.queue.push(func);
+	}
+
+	Quirk.ready.queue = []
+
 	//once document is loaded, loop through the wait list and execute and functions.
 	document.onreadystatechange = function()
 	{
 		if(document.readyState == 'complete')
 		{
-			for(var i=0; i < Quirk.ready.wait.length; i++)
-				Quirk.ready.wait[i]();
+			for(var i=0; i < Quirk.ready.queue.length; i++)
+				Quirk.ready.queue[i]();
 		}
 	}
 
-   	if(!window.$q){window.$q = window.Quirk = window.Q = Quirk;}
+	if(!window.$q || !window.Quirk){window.$q = window.Quirk = window.Q = Quirk;}
 })();
 
