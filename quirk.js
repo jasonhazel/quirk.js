69edc9a9412b29843bc5bea73e9f6c29147d6ee9
Jason Hazel <me@jasonhazel.com>
2012-07-13 11:21:56 -0400

    Added event listeners, added A specific methods.

--- quirk.js
+++ quirk.js
@@ -23,6 +23,24 @@
 		return new qObj(e);
 	}
 
+	// add a function to the wait queue.
+	Quirk.ready = function(func)
+	{
+		Quirk.ready.queue.push(func);
+	}
+
+	Quirk.ready.queue = []
+
+	//once document is loaded, loop through the wait list and execute and functions.
+	document.onreadystatechange = function()
+	{
+		if(document.readyState == 'complete')
+		{
+			for(var i=0; i < Quirk.ready.queue.length; i++)
+				Quirk.ready.queue[i]();
+		}
+	}
+
 	Quirk.select = {
 		cache : {}, 
 		id : function(selector)
@@ -33,8 +51,7 @@
 			return this.cache[selector];
 		},
 		class : function(selector){
-
-
+			// TODO
 		},
 		new : function(selector){
 			if(selector.charAt(selector.length - 1) == '>')
@@ -50,6 +67,32 @@
 	var qObj = function(element)
 	{
 		this.element = element;
+		this.type = element.tagName.toLowerCase();
+		this.events = []
+
+		// kinda hacky right now.
+		// not all objects require all methods.  Here we provided object specific methods.
+		switch(this.type)
+		{
+			case 'a':
+				this.href 	= function(url) 	{ this.attr('href',url); 		return this;}
+				this.target = function(target) 	{ this.attr('target',target); 	return this;}
+			break;
+		}
+
+		this.on = function(action, func)
+		{
+			this.events[action] = func;
+
+			this.element.addEventListener(action, this.events[action], false);
+			return this;
+		}
+
+		this.off = function(action)
+		{
+			this.element.removeEventListener(action, this.events[action]);
+		}
+
 		this.text = function()
 		{
 			switch(arguments.length)
@@ -92,12 +135,17 @@
 		{
 			if(child instanceof qObj)
 				this.element.appendChild(child.element);
+			else
+				this.element.appendChild(child);
 		}
 		
 		this.appendTo = function(parent)
 		{
-			if(child instanceof qObj)
+			console.debug(parent);
+			if(parent instanceof qObj)
 				parent.element.appendChild(this.element);
+			else
+				parent.appendChild(this.element);
 		}
 
 		this.hide = function()
@@ -117,24 +165,6 @@
 
 		}
 
-	}
-
-	// add a function to the wait queue.
-	Quirk.ready = function(func)
-	{
-		Quirk.ready.queue.push(func);
-	}
-
-	Quirk.ready.queue = []
-
-	//once document is loaded, loop through the wait list and execute and functions.
-	document.onreadystatechange = function()
-	{
-		if(document.readyState == 'complete')
-		{
-			for(var i=0; i < Quirk.ready.queue.length; i++)
-				Quirk.ready.queue[i]();
-		}
 	}
 
 	if(!window.$q || !window.Quirk){window.$q = window.Quirk = window.Q = Quirk;}
