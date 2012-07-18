7bebb5addeadfbd206b162097a9368258b27e658
Jason Hazel <me@jasonhazel.com>
2012-07-18 11:59:52 -0400

    formatting

--- quirk.js
+++ quirk.js
@@ -1,13 +1,10 @@
 (function(){
-	var Quirk = function(selector)
-	{
-		if(selector != undefined)
-		{
+	var Quirk = function(selector){
+		if(selector != undefined){
 			var type = selector.charAt(0);
 			var element = selector.substring(1,selector.length);
 
-			switch(type)
-			{
+			switch(type){
 				case '#':
 					return new QuirkObject(document.getElementById(element));
 				break;
@@ -31,22 +28,27 @@
 		}
 	}
 
-
-	
-	Quirk.ready 		= function(func) 		{ Quirk.ready.queue.push(func);	}
-	Quirk.ready.queue 	= []
+	Quirk.ready 		= function(func){
+		if(Quirk.ready.queue == undefined)
+			Quirk.ready.queue = []
+		Quirk.ready.queue.push(func);
+	}
 	
 	//utility methods
-	Quirk.isArray 		= function(variable)	{ return (variable instanceof Array); }
-	Quirk.isNodeList 	= function(variable)	{ return (variable instanceof NodeList); }
-	Quirk.isList 		= function(variable) 	{ return Quirk.isArray(variable) || Quirk.isNodeList(variable); }
+	Quirk.isArray 		= function(variable){ 
+		return (variable instanceof Array); 
+	}
 
+	Quirk.isNodeList 	= function(variable){ 
+		return (variable instanceof NodeList); 
+	}
+	Quirk.isList 		= function(variable){ 
+		return Quirk.isArray(variable) || Quirk.isNodeList(variable); 
+	}
 
-	var QuirkCollection = function(elements)
-	{
+	var QuirkCollection = function(elements){
 		var collection = []
-		if(Quirk.isNodeList(elements))
-		{
+		if(Quirk.isNodeList(elements)){
 			for(var i=0;i<elements.length;i++)
 				collection.push(new QuirkObject(elements[i]));
 		}
@@ -54,56 +56,50 @@
 		this.position = 0;
 		this.length = collection.length;
 
-		this.each = function(func)
-		{
+		this.each = function(func){
 			for(item in collection)
 				func(collection[item]);
-
 			return this;
 		}
 
-		this.next = function()
-		{
+		this.next = function(){
 			return collection[this.position++] || null;
 		}
 
-		this.reset = function()
-		{
+		this.reset = function(){
 			this.position = 0;
 			return this;
 		}
 
-		this.remove = function()
-		{
+		this.remove = function(){
 			this.each(function(e){e.remove();})
 			return this;
 		}
 	}
 
 
-	var QuirkObject = function(e)
-	{
+	var QuirkObject = function(e){
 		var _element = e;
 		var _type 	= _element.tagName.toLowerCase() || undefined;
-		var _events 	= []
-		
+		var _events 	= []		
 		this.element = _element;
 
-
-		switch(_type)
-		{
+		switch(_type){
 			case 'a': // only for anchors.
 				this.href 	= function(url)		{ return this.attr('href',url);				}
 				this.target = function(target)	{ return this.attr('target',target);		} 
 			break;
 		}
 		
-		this.data 	= function(key, value)		{ return this.attr('data-' + key, value); 	}
-		this.id 	= function(id)				{ return this.attr('id', id);				}
+		this.data 	= function(key, value){ 
+			return this.attr('data-' + key, value); 	
+		}
+		this.id 	= function(id){ 
+			return this.attr('id', id);				
+		}
 		
 		//event listener
-		this.on = function(action, func)
-		{
+		this.on = function(action, func){
 			if(_events[action] == undefined)
 				_events[action] = []
 
@@ -113,102 +109,83 @@
 		}
 
 		//I don't like calling this off. needs a better name.
-		this.off = function(action) 
-		{
+		this.off = function(action){
 			for(func in _events[action])
 				_element.removeEventListener(action, _events[action][func]);
 			return this;
 		}
 
-		this.attr = function(key, value)
-		{
+		this.attr = function(key, value){
 			if(key != undefined && value != undefined)
 				_element.setAttribute(key, value);
 			else if (key != undefined)
 				return _element.getAttribute(key);
-
 			return this;
 		}
 
-		this.addClass = function(className)
-		{
+		this.addClass = function(className){
 			_element.classList.add(className);
 			return this;
 		}
 
-		this.removeClass = function(className)
-		{
+		this.removeClass = function(className){
 			_element.classList.remove(className);
 			return this;;
 		}
 
-		this.toggleClass = function(className)
-		{
+		this.toggleClass = function(className){
 			_element.classList.toggle(className);
 			return this;
 		}
 
-		this.hasClass = function(className)
-		{
+		this.hasClass = function(className){
 			return _element.classList.contains(className);
 		}
 
-		this.hasProperty = function(property)
-		{
+		this.hasProperty = function(property){
 			return _element.hasOwnProperty(property);
 		}
 
-		this.text = function(value)
-		{
+		this.text = function(value){
 			var field = (this.hasProperty('value') ? 'value' : 'innerHTML');
 			if(value != undefined)
 				_element[field] = value;
 			else
 				return _element[field];
-			
 			return this;
 		}
 
-		this.remove = function()
-		{
+		this.remove = function(){
 			_element.parentNode.removeChild(_element);
 		}
 
-		this.append = function(child)
-		{
-
+		this.append = function(child){
 			_element.appendChild((child instanceof QuirkObject ? child.element : child));
 			return this;
 		}
 
-		this.appendTo = function(parent)
-		{
+		this.appendTo = function(parent){
 			(parent instanceof QuirkObject ? parent.element : parent).appendChild(this.element);
 		}
 
-		this.prepend = function(child)
-		{
+		this.prepend = function(child){
 			_element.insertBefore((child instanceof QuirkObject ? child.element : child) , _element.firstChild);
 		}
 
-		this.prependTo = function(parent)
-		{
+		this.prependTo = function(parent){
 			var parent = (parent instanceof QuirkObject ? parent.element : parent)
 			parent.insertBefore(_element, parent.firstChild);
 		}
 
-		this.hide = function()
-		{
+		this.hide = function(){
 			_element.style.visibility = 'hidden';
 		}
 
-		this.show = function()
-		{
+		this.show = function(){
 			_element.style.visibility = 'visible';
 		}
 
-		this.toggle = function()
-		{
+		this.toggle = function(){
 			_element.style.visibility = (_element.style.visibility == 'hidden' ? 'visible' : 'hidden');
 		}
 	}
